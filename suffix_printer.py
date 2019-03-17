aad01caf6ec5e0e01fb878c2427927353e4e7c63
Justin Angel <justinangel86@gmail.com>
2019-03-16 20:47:47 -0400

    updates

--- suffix_printer.py
+++ suffix_printer.py
@@ -1,13 +1,20 @@
+#!/usr/bin/env python3
+
 from sys import stdout,stderr
 
-def suffix(s,suf="[+]"):
+DEF = DEFAULT = '[+]'
+NOT = NOTICE  = '[-]'
+WAR = WARNING = '[!]'
+ERR = ERROR   = '[ERROR]' 
+
+def suffix(s,suf=DEFAULT):
     '''
     Suffix and return a string.
     '''
 
     return f"{suf} {s}"
 
-def suffix_print(s,suf="[+]",file=stdout,end="\n"):
+def suffix_print(s,suf=DEFAULT,file=stdout,end="\n"):
     '''
     Suffix and print a string to stdout.
     '''
@@ -28,7 +35,6 @@ def suffix_print_stderror(*args, **kwargs):
 
     suffix_print(file=stderr,*args,**kwargs)
 
-# SHORTCUTS ARE SHORT
 sprint = suffix_print
 csprint = clean_suffix_print
 esprint = suffix_print_stderror
